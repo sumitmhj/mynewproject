138c01f9fc9222e0c501f6620cabf61a75f2ebc7
Sammit <samitshrestha4@gmail.com>
2021-09-19 09:27:10 +0200

    student registation page added

---
+++ registation.php
@@ -0,0 +1,46 @@
+
+
+
+
+<!DOCTYPE>  
+<html>  
+	<head>   
+	  <script src="myscript.js"></script>
+	  <link rel="stylesheet" type="text/css" href="mystyle.css"> 
+	  <title>Sumit</title>  
+	</head>  
+<body> 
+
+	
+
+<div>
+	<h2>Student Registration Form</h2>
+	<form  id="form" action="submit.php" method="post">  
+		<label class="formText" for="firstname">First Name: </label> <br>  
+		      <input type="text" id="fname" class="ftext" name="firstname" /> <br/> 
+
+		 <label class="formText" for="middlename">Middle Name: </label>  <br>
+		      <input type="text" id-"middlename" class="ftext" name="middlename"/> <br/>
+
+		<label class="formText" for="lastname">Last Name: </label>  <br>
+		      <input type="text" id-"lname" class="ftext" name="lastname"/> <br/>
+
+		<label class="formText" for="emailid">Email id: </label>  <br>
+		      <input type="text" class="ftext" name="emailid"/> <br/> 
+
+        <label class="formText" for="address">Address: </label>  <br>
+		      <input type="text" class="ftext"id="addre" name="addrees"/> <br/> 
+
+		<label class="formText" for="emailid">Roll no: </label>  <br>
+		      <input type="text" class="ftext" name="rollno"/> <br/>  
+
+	    <label class="formText" for="contactno">Phone No: </label> <br>
+		      <input type="text" class="ftext" name="phoneno"/> <br/>  
+		
+		
+	    <button type="submit">Submit</button> 
+	</form>  
+</div>
+
+</body>  
+</html>  
---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php 
+         require 'connection.php';
+
+			$firstname = $_POST['firstname'];
+			$middlename = $_POST['middlename'];
+			$lastname = $_POST['lastname'];
+			$emailid = $_POST['emailid'];
+			$addrees = $_POST['addrees'];
+			$rollno = $_POST['rollno'];
+			$phoneno = $_POST['phoneno'];
+			echo $firstname;
+
+
+	$query = "insert into student(firstname, middlename, lastname, emailid, addrees, rollno, phoneno)
+		values('$firstname','$middlename', '$lastname', '$emailid', '$addrees', '$rollno', '$phoneno')";
+
+   $run = mysqli_query($conn, $query) or die(mysqli_error());
+
+   if($run){
+   	 echo "Data inserted";
+   } else{
+   	echo "Unable";
+   }
+   
+	$conn -> close();
+
+	
+	?>
